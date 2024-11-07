c41d6c537278904698c558e185712f328525ca52
Heberto Mayorquin <h.mayorquin@gmail.com>
2024-11-07 10:53:13 -0600

    use yoon function

--- src/dicarlo_lab_to_nwb/conversion/convert_session.py
+++ src/dicarlo_lab_to_nwb/conversion/convert_session.py
@@ -1,5 +1,6 @@
 """Primary script to run to convert an entire session for of data using the NWBConverter."""
 
+import re
 import time
 from datetime import datetime
 from pathlib import Path
@@ -33,54 +34,55 @@ def convert_session_to_nwb(
     intan_file_path: str | Path,
     mworks_processed_file_path: str | Path,
     stimuli_folder: str | Path,
-    output_dir_path: str | Path | None = None,
+    output_dir_path: str | Path,
     stub_test: bool = False,
     verbose: bool = False,
     add_thresholding_events: bool = False,
     add_psth: bool = False,
     stimuli_are_video: bool = False,
-    add_stimuli_media_to_nwb: bool = False,
     thresholindg_pipeline_kwargs: dict = None,
     psth_kwargs: dict = None,
     ground_truth_time_column: str = "samp_on_us",
-    add_amplifier_data_to_nwb: bool = False,
+    add_raw_amplifier_data: bool = False,
     probe_info_path: str | Path | None = None,
     add_psth_in_pipeline_format_to_nwb: bool = True,
-):
+) -> Path:
+
     if verbose:
         total_start = time.time()
         start = time.time()
 
-    if output_dir_path is None:
-        output_dir_path = Path.home() / "conversion_nwb"
-
     project_name = session_metadata["project_name"]
-    session_date = session_metadata["session_date"]
-    session_time = session_metadata["session_time"]
     subject = session_metadata["subject"]
-    type_of_data = session_metadata.get("type_of_data", "-")  # Either session or normalizer
-    pipeline_version = session_metadata.get("pipeline_version", "-")
-    project_name_camel_case = "".join([word.capitalize() for word in project_name.split("_")])
+    recording_id = session_metadata.get(
+        "recording_id", ""
+    )  # recording_id contains stimulus name, date, and time of the recording
+    pipeline_version = session_metadata.get("pipeline_version", "")
+
+    recording_id_match = re.search(r"_(\d{6})_(\d{6})$", recording_id)
+    if recording_id_match:
+        # date_str, time_str = recording_id_match.groups()
+        # dt = datetime.strptime(f'20{date_str} {time_str}', '%Y%m%d %H%M%S')
+        session_date = recording_id_match.group(1)
+        session_time = recording_id_match.group(2)
+    else:
+        assert False, f"Recording ID {recording_id} does not match the expected format"
 
     output_dir_path = Path(output_dir_path)
     if stub_test:
         output_dir_path = output_dir_path / "nwb_stub"
-
-    output_dir_path = output_dir_path / project_name
     output_dir_path.mkdir(parents=True, exist_ok=True)
 
-    # Set the file name for the NWB file
-    session_id = f"{subject}_{project_name_camel_case}_tresholded_{session_date}_{session_time}_{type_of_data}_{pipeline_version}"
-
+    # session_id defines the name of the NWB file
+    session_id = f"{project_name}_{subject}_{recording_id}_{pipeline_version}_thresholded"
     nwbfile_path = output_dir_path / f"{session_id}.nwb"
 
     if verbose:
-        print("=============================================")
         print(f"Converting session: {session_id}")
 
     conversion_options = dict()
 
-    if add_amplifier_data_to_nwb:
+    if add_raw_amplifier_data:
         ecephys_interface = IntanRecordingInterface(file_path=intan_file_path, ignore_integrity_checks=True)
         attach_probe_to_recording(recording=ecephys_interface.recording_extractor)
         if stub_test:
@@ -101,39 +103,35 @@ def convert_session_to_nwb(
     # Behavioral Trials Interface
     behavioral_trials_interface = BehavioralTrialsInterface(file_path=mworks_processed_file_path)
     conversion_options["Behavior"] = dict(stub_test=stub_test, ground_truth_time_column=ground_truth_time_column)
+    # Add Stimuli Interface
+    if stimuli_are_video:
+        stimuli_interface = StimuliVideoInterface(
+            file_path=mworks_processed_file_path,
+            folder_path=stimuli_folder,
+            image_set_name=session_id,
+            # video_copy_path=output_dir_path / "videos",
+            video_copy_path=None,  # Add a path if videos should be copied
+            verbose=verbose,
+        )
+    else:
+        stimuli_interface = StimuliImagesInterface(
+            file_path=mworks_processed_file_path,
+            folder_path=stimuli_folder,
+            image_set_name=session_id,
+            verbose=verbose,
+        )
+    conversion_options["Stimuli"] = dict(stub_test=stub_test, ground_truth_time_column=ground_truth_time_column)
 
     # Build the converter pipe with the previously defined data interfaces
     data_interfaces_dict = {
         "Ecephys": ecephys_interface,
         "Behavior": behavioral_trials_interface,
+        "Stimuli": stimuli_interface,
     }
-
-    # Add Stimuli Interface
-    if add_stimuli_media_to_nwb:
-        if stimuli_are_video:
-            stimuli_interface = StimuliVideoInterface(
-                file_path=mworks_processed_file_path,
-                folder_path=stimuli_folder,
-                image_set_name=project_name,
-                # video_copy_path=output_dir_path / "videos",
-                video_copy_path=None,  # Add a path if videos should be copied
-                verbose=verbose,
-            )
-        else:
-            stimuli_interface = StimuliImagesInterface(
-                file_path=mworks_processed_file_path,
-                folder_path=stimuli_folder,
-                image_set_name=project_name,
-                verbose=verbose,
-            )
-
-        conversion_options["Stimuli"] = dict(stub_test=stub_test, ground_truth_time_column=ground_truth_time_column)
-        data_interfaces_dict["Stimuli"] = stimuli_interface
-
     converter_pipe = ConverterPipe(data_interfaces=data_interfaces_dict, verbose=verbose)
 
     # Parse the string into a datetime object
-    datetime_str = f"{session_date} {session_time}"
+    datetime_str = f"20{session_date} {session_time}"
     datetime_format = "%Y%m%d %H%M%S"
     session_start_time = datetime.strptime(datetime_str, datetime_format).replace(tzinfo=ZoneInfo("US/Eastern"))
 
@@ -167,7 +165,6 @@ def convert_session_to_nwb(
             print(f"Conversion took {conversion_time_seconds / 60:.2f} minutes")
         else:
             print(f"Conversion took {conversion_time_seconds / 60 / 60:.2f} hours")
-        print("\n")
 
     # Calculate thresholding events
     if add_thresholding_events:
@@ -183,7 +180,7 @@ def convert_session_to_nwb(
         noise_threshold = thresholindg_pipeline_kwargs.get("noise_threshold", None)
         job_kwargs = thresholindg_pipeline_kwargs.get("job_kwargs", None)
 
-        if add_amplifier_data_to_nwb:
+        if add_raw_amplifier_data:
             file_path = nwbfile_path
         else:
             file_path = intan_file_path
@@ -219,7 +216,6 @@ def convert_session_to_nwb(
                 print(f"Thresholding events took {thresholding_time / 60:.2f} minutes")
             else:
                 print(f"Thresholding events took {thresholding_time / 60 / 60:.2f} hours")
-            print("\n")
 
     # Add PSTH
     if add_thresholding_events and add_psth:
@@ -256,16 +252,17 @@ def convert_session_to_nwb(
                 print(f"PSTH calculation took {psth_time / 60:.2f} minutes")
             else:
                 print(f"PSTH calculation took {psth_time / 60 / 60:.2f} hours")
-            print("\n")
 
     if verbose:
         total_stop = time.time()
-        total_script_time = total_stop - total_start
-        if total_script_time <= 60 * 3:
-            print(f"Total script took {total_script_time:.2f} seconds")
-        elif total_script_time <= 60 * 60:
-            print(f"Total script took {total_script_time / 60:.2f} minutes")
+        total_scrip_time = total_stop - total_start
+        if total_scrip_time <= 60 * 3:
+            print(f"Total script took {total_scrip_time:.2f} seconds")
+        elif total_scrip_time <= 60 * 60:
+            print(f"Total script took {total_scrip_time / 60:.2f} minutes")
         else:
-            print(f"Total script took {total_script_time / 60 / 60:.2f} hours")
+            print(f"Total script took {total_scrip_time / 60 / 60:.2f} hours")
 
         print("\n \n")
+
+    return nwbfile_path
