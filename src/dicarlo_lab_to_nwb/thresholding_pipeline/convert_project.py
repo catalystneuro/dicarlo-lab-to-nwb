79fee3137842010fa0421df2308a296091403f98
yoonbai <yhb@mit.edu>
2024-11-07 01:08:43 -0500

    adding project config file and quality control for daily sessions

---
+++ src/dicarlo_lab_to_nwb/thresholding_pipeline/convert_project.py
@@ -0,0 +1,211 @@
+import yaml
+import time
+from datetime import datetime
+from pathlib import Path
+from dicarlo_lab_to_nwb.thresholding_pipeline.data_locator import locate_session_paths
+from dicarlo_lab_to_nwb.thresholding_pipeline.parse_mworks_RSVP import parse_mworks_file
+from dicarlo_lab_to_nwb.thresholding_pipeline.convert_session import convert_session_to_nwb
+from pynwb import NWBHDF5IO
+import numpy as np
+from dicarlo_lab_to_nwb.conversion.quality_control.latency import get_unit_latencies_from_reliabilities
+from dicarlo_lab_to_nwb.conversion.quality_control.reliability import get_NRR, get_p_values
+import pandas as pd
+
+# Recursive function to parse YAML content
+def parse_yaml_recursively(data):
+    # Check if the data is a dictionary
+    if isinstance(data, dict):
+        parsed_data = {}
+        for key, value in data.items():
+            parsed_data[key] = parse_yaml_recursively(value)
+        return parsed_data
+    
+    # Check if the data is a list
+    elif isinstance(data, list):
+        return [parse_yaml_recursively(item) for item in data]
+    
+    # If it's neither dict nor list, it's a base case (string, int, etc.)
+    else:
+        return data
+
+
+def convert_project_sessions(project_config_path: str | Path):
+    project_config_path = Path(project_config_path)
+    # assert if project_config_path is not a .yaml file
+    assert project_config_path.exists(), f"File {project_config_path} does not exist"
+
+    with open(project_config_path, "r") as file:
+        try:
+            yaml_content = yaml.safe_load(file)
+        except yaml.YAMLError as exc:
+            print(exc)
+    parsed_result = parse_yaml_recursively(yaml_content)
+        
+    # GLOBAL VARIABLES
+    # TODO: put this in a config file
+    subject = "Apollo"
+    pipeline_version = "DiLorean"
+
+    stub_test = False
+    verbose = True
+    add_thresholding_events = True
+    add_psth = True
+    stimuli_are_video = False
+    add_raw_amplifier_data = False
+    add_psth_in_pipeline_format_to_nwb = True
+
+    thresholindg_pipeline_kwargs = {
+        "f_notch": 60.0,  # Frequency for the notch filter
+        "bandwidth": 10.0,  # Bandwidth for the notch filter
+        "f_low": 300.0,  # Low cutoff frequency for the bandpass filter
+        "f_high": 6000.0,  # High cutoff frequency for the bandpass filter
+        "noise_threshold": 3,  # Threshold for detection in the thresholding algorithm
+    }
+
+    # Ten bins starting 200 ms before the stimulus and spanning 400 ms
+    psth_kwargs = {"bins_span_milliseconds": 500, "num_bins": 50, "milliseconds_from_event_to_first_bin": -200.0}
+    # psth_kwargs = {"bins_span_milliseconds": 400, "num_bins": 10, "milliseconds_from_event_to_first_bin": -200.0}
+
+    # This is the ground truth time column for the stimuli in the mworks csv file
+    ground_truth_time_column = "samp_on_us"
+
+
+    # project-level information
+    project_info = parsed_result.get("project", {})
+    project = project_info.get("name")
+    subject = project_info.get("subject")
+    nwb_output_folder = project_info.get("nwb_output_folder")
+    project_version = project_info.get("project_version")
+    pipeline_version = project_info.get("pipeline_version")
+    mworks_version = project_info.get("mworks_version")
+
+    # Merged NWB file will be saved in the nwb_output_folder
+    nwb_output_folder = Path(nwb_output_folder)
+    nwb_output_folder.mkdir(parents=True, exist_ok=True)
+
+    # experiment-level information
+    experiments = project_info.get("experiments", [])
+    for experiment in experiments:
+        experiment_date = experiment.get("date")
+        # intermediate NWB files from each day will be saved in the nwb_output_folder
+        experiment_output_folder = nwb_output_folder / f"{experiment_date}"
+        experiment_output_folder.mkdir(parents=True, exist_ok=True)
+        print(f"\nExperiment Date: {experiment_date}-------------------\n")
+
+        # session-level information
+        sessions = experiment.get("sessions", [])
+        for session in sessions:
+            stimulus_name = session.get("stimulus_name")
+            stimulus_folder = Path(session.get("stimulus_folder"))
+            mworks_folder = Path(session.get("mworks_folder"))
+            data_folder = Path(session.get("data_folder"))
+            intan_file_path = data_folder / "info.rhd"
+            assert intan_file_path.exists(), f"{intan_file_path} does not exist"
+            notes = session.get("notes")
+
+            print(f"  Session Stimulus Name: {stimulus_name}")
+            print(f"  - Stimulus Folder: {stimulus_folder}")
+            print(f"  - MWorks Folder: {mworks_folder}")
+            print(f"  - Data Folder: {data_folder}")
+            print(f"  - Notes: {notes}\n")
+            
+            # session info
+            session_metadata = {
+                "project_name": project,
+                "subject": subject,
+                "recording_id": data_folder.name,
+                "pipeline_version": pipeline_version,
+            }
+
+            # Extract stimulus/behavioral events from MWorks files
+            mworks_processed_file_path = parse_mworks_file(mworks_folder, data_folder, mworks_folder) # save MWorks results in the same folder
+            print(f"  - MWorks processed file saved to: {mworks_processed_file_path}")
+
+            session_nwb_filepath = convert_session_to_nwb(
+                session_metadata=session_metadata,
+                intan_file_path=intan_file_path,
+                mworks_processed_file_path=mworks_processed_file_path,
+                stimuli_folder=stimulus_folder,
+                thresholindg_pipeline_kwargs=thresholindg_pipeline_kwargs,
+                psth_kwargs=psth_kwargs,
+                output_dir_path=experiment_output_folder,
+                stub_test=stub_test,
+                verbose=verbose,
+                add_thresholding_events=add_thresholding_events,
+                add_psth=add_psth,
+                stimuli_are_video=stimuli_are_video,
+                ground_truth_time_column=ground_truth_time_column,
+                add_raw_amplifier_data=add_raw_amplifier_data,
+                add_psth_in_pipeline_format_to_nwb=add_psth_in_pipeline_format_to_nwb,
+            )
+
+            # add quality control if stimulus_name contains "normalizer"
+            if "normalizer" in stimulus_name:
+                with NWBHDF5IO(session_nwb_filepath, mode='r') as io:
+                    nwbfile = io.read()
+                    psth = nwbfile.scratch["psth_pipeline_format"].data[:]
+                    n_units, n_stimuli, n_reps, n_timebins = psth.shape
+                    df = nwbfile.electrodes.to_dataframe()
+                    channel_names = df["channel_name"].values
+
+                    binned_spikes = nwbfile.processing['ecephys']['BinnedAlignedSpikesToStimulus']
+                    psth_timebin_ms = binned_spikes.bin_width_in_milliseconds
+                    psth_0 = binned_spikes.milliseconds_from_event_to_first_bin 
+                    psth_1 = psth_0 + psth_timebin_ms * n_timebins - psth_timebin_ms
+                    psth_timebins_s = np.linspace(psth_0, psth_1, n_timebins) / 1e3
+                    latencies_s = get_unit_latencies_from_reliabilities(psth, psth_timebins_s)
+                
+                    trial_df = nwbfile.intervals["trials"].to_dataframe()
+                    stim_duration_s = trial_df["stimuli_presentation_time_ms"].unique()[0] / 1e3
+                    stim_size_deg = trial_df["stimulus_size_degrees"].unique()[0]
+
+                    # integrate spike counts over stimulus presentation duration + response latency
+                    psth_stim_onset_s = 0
+                    rates = np.full((n_units, n_stimuli, n_reps), np.nan)
+                    mean_rates = np.full(n_units, np.nan)
+                    for i in range(n_units):
+                        intg_window_s_0 = psth_stim_onset_s + latencies_s[i]
+                        intg_window_s_1 = intg_window_s_0 + stim_duration_s
+                        intg_window = [intg_window_s_0, intg_window_s_1]
+                        intg_window_size_s = np.diff(intg_window)[0]
+                        intg_window_idx = [np.argmin(np.abs(psth_timebins_s - t)) for t in intg_window]
+
+                        psth_unit = psth[i]
+                        rates[i,:,:] = np.sum(psth_unit[..., intg_window_idx[0]:intg_window_idx[1]], axis=-1) / intg_window_size_s
+                        # average rates across all dimensions except the first one
+                        mean_rates[i] = np.nanmean(rates[i])
+                
+                    # p values
+                    p_values = get_p_values(rates)
+                    p_thresh = 0.05
+                    valid_units = p_values < p_thresh
+                    print(f"with p < {p_thresh}: N={np.sum(valid_units)} out of {len(p_values)} units")
+                    print(f"mean latencies (valid units): {np.nanmean(latencies_s[valid_units])}")
+
+                    # reliabilities
+                    n_samples = n_reps // 2
+                    rhos_samples = get_NRR(rates, n_reps=n_samples)
+                    rhos_mean_values = np.nanmean(rhos_samples, axis=1)
+                    print(f"half-split reliability (above 0.5) : {np.sum(rhos_mean_values>0.5)}")
+                    srr_samples = get_NRR(rates, n_reps=2, correction=False)
+                    srr_mean_values = np.nanmean(srr_samples, axis=-1)
+                    print(f"SRR mean (valid units): {np.mean(srr_mean_values[valid_units])}")
+                
+                    # save results to a dataframe
+                    df = pd.DataFrame({
+                        "channel_name": channel_names,
+                        "p_value": p_values,
+                        "valid_unit": valid_units,
+                        "mean_rate": mean_rates,
+                        "response_latency_ms": latencies_s,
+                        "half_split_reliability": rhos_mean_values,
+                        "single_repeat_reliability": srr_mean_values,
+                    })
+                    csv_filepath = session_nwb_filepath.parent / f"{nwbfile.session_id}_QC.csv"
+                    df.to_csv(csv_filepath, index=False)
+
+if __name__ == '__main__':
+    example_project_config = Path(__file__).parent / "project_config.yaml"
+    convert_project_sessions(example_project_config)
+        
+    
---
+++ src/dicarlo_lab_to_nwb/thresholding_pipeline/convert_session.py
@@ -0,0 +1,266 @@
+"""Primary script to run to convert an entire session for of data using the NWBConverter."""
+
+import time
+import re
+from datetime import datetime
+from pathlib import Path
+from zoneinfo import ZoneInfo
+
+from neuroconv import ConverterPipe
+from neuroconv.datainterfaces import IntanRecordingInterface
+from neuroconv.utils import dict_deep_update, load_dict_from_file
+
+from dicarlo_lab_to_nwb.conversion.behaviorinterface import BehavioralTrialsInterface
+from dicarlo_lab_to_nwb.conversion.pipeline import (
+    calculate_thresholding_events,
+    write_thresholding_events_to_nwb,
+)
+from dicarlo_lab_to_nwb.conversion.probe import (
+    UtahArrayProbeInterface,
+    attach_probe_to_recording,
+)
+from dicarlo_lab_to_nwb.conversion.psth import (
+    write_binned_spikes_to_nwbfile,
+    write_psth_pipeline_format_to_nwbfile,
+)
+from dicarlo_lab_to_nwb.conversion.stimuli_interface import (
+    StimuliImagesInterface,
+    StimuliVideoInterface,
+)
+
+
+def convert_session_to_nwb(
+    session_metadata: dict,
+    intan_file_path: str | Path,
+    mworks_processed_file_path: str | Path,
+    stimuli_folder: str | Path,
+    output_dir_path: str | Path,
+    stub_test: bool = False,
+    verbose: bool = False,
+    add_thresholding_events: bool = False,
+    add_psth: bool = False,
+    stimuli_are_video: bool = False,
+    thresholindg_pipeline_kwargs: dict = None,
+    psth_kwargs: dict = None,
+    ground_truth_time_column: str = "samp_on_us",
+    add_raw_amplifier_data: bool = False,
+    probe_info_path: str | Path | None = None,
+    add_psth_in_pipeline_format_to_nwb: bool = True,
+)->Path:
+    
+    if verbose:
+        total_start = time.time()
+        start = time.time()
+
+    project_name = session_metadata["project_name"]
+    subject = session_metadata["subject"]
+    recording_id = session_metadata.get("recording_id", "") # recording_id contains stimulus name, date, and time of the recording
+    pipeline_version = session_metadata.get("pipeline_version", "")
+
+    recording_id_match = re.search(r"_(\d{6})_(\d{6})$", recording_id)
+    if recording_id_match:
+        # date_str, time_str = recording_id_match.groups()
+        # dt = datetime.strptime(f'20{date_str} {time_str}', '%Y%m%d %H%M%S')
+        session_date = recording_id_match.group(1)
+        session_time = recording_id_match.group(2)
+    else: 
+        assert False, f"Recording ID {recording_id} does not match the expected format"
+    
+    output_dir_path = Path(output_dir_path)
+    if stub_test:
+        output_dir_path = output_dir_path / "nwb_stub"
+    output_dir_path.mkdir(parents=True, exist_ok=True)
+
+    # session_id defines the name of the NWB file
+    session_id = f"{project_name}_{subject}_{recording_id}_{pipeline_version}_thresholded"
+    nwbfile_path = output_dir_path / f"{session_id}.nwb"
+
+    if verbose:
+        print(f"Converting session: {session_id}")
+
+    conversion_options = dict()
+
+    if add_raw_amplifier_data:
+        ecephys_interface = IntanRecordingInterface(file_path=intan_file_path, ignore_integrity_checks=True)
+        attach_probe_to_recording(recording=ecephys_interface.recording_extractor)
+        if stub_test:
+            intan_recording = ecephys_interface.recording_extractor
+            duration = intan_recording.get_duration()
+            end_time = min(10.0, duration)
+            stubed_recording = ecephys_interface.recording_extractor.time_slice(start_time=0, end_time=end_time)
+            ecephys_interface.recording_extractor = stubed_recording
+
+        conversion_options["Ecephys"] = dict(
+            iterator_opts={"display_progress": True, "buffer_gb": 5},
+        )
+    else:
+        # This path adds the geometry of the probe as the electrodes table so the units can be linked
+        # Add Utah Array Probe Interface, pass a path if a different geometry is used
+        ecephys_interface = UtahArrayProbeInterface(file_path=probe_info_path)
+
+    # Behavioral Trials Interface
+    behavioral_trials_interface = BehavioralTrialsInterface(file_path=mworks_processed_file_path)
+    conversion_options["Behavior"] = dict(stub_test=stub_test, ground_truth_time_column=ground_truth_time_column)
+    # Add Stimuli Interface
+    if stimuli_are_video:
+        stimuli_interface = StimuliVideoInterface(
+            file_path=mworks_processed_file_path,
+            folder_path=stimuli_folder,
+            image_set_name=session_id,
+            # video_copy_path=output_dir_path / "videos",
+            video_copy_path=None,  # Add a path if videos should be copied
+            verbose=verbose,
+        )
+    else:
+        stimuli_interface = StimuliImagesInterface(
+            file_path=mworks_processed_file_path,
+            folder_path=stimuli_folder,
+            image_set_name=session_id,
+            verbose=verbose,
+        )
+    conversion_options["Stimuli"] = dict(stub_test=stub_test, ground_truth_time_column=ground_truth_time_column)
+
+    # Build the converter pipe with the previously defined data interfaces
+    data_interfaces_dict = {
+        "Ecephys": ecephys_interface,
+        "Behavior": behavioral_trials_interface,
+        "Stimuli": stimuli_interface,
+    }
+    converter_pipe = ConverterPipe(data_interfaces=data_interfaces_dict, verbose=verbose)
+
+    # Parse the string into a datetime object
+    datetime_str = f"20{session_date} {session_time}"
+    datetime_format = "%Y%m%d %H%M%S"
+    session_start_time = datetime.strptime(datetime_str, datetime_format).replace(tzinfo=ZoneInfo("US/Eastern"))
+
+    # Add datetime to conversion
+    metadata = converter_pipe.get_metadata()
+    metadata["NWBFile"]["session_start_time"] = session_start_time
+    metadata["NWBFile"]["session_id"] = session_id
+
+    # Update default metadata with the editable in the corresponding yaml file
+    editable_metadata_path = Path(__file__).parent / "metadata.yaml"
+    editable_metadata = load_dict_from_file(editable_metadata_path)
+    metadata = dict_deep_update(metadata, editable_metadata)
+
+    subject_metadata = metadata["Subject"]
+    subject_metadata["subject_id"] = f"{subject}"
+
+    # Run conversion, this adds the basic data to the NWBFile
+    converter_pipe.run_conversion(
+        metadata=metadata,
+        nwbfile_path=nwbfile_path,
+        conversion_options=conversion_options,
+        overwrite=True,
+    )
+
+    if verbose:
+        stop_time = time.time()
+        conversion_time_seconds = stop_time - start
+        if conversion_time_seconds <= 60 * 3:
+            print(f"Conversion took {conversion_time_seconds:.2f} seconds")
+        elif conversion_time_seconds <= 60 * 60:
+            print(f"Conversion took {conversion_time_seconds / 60:.2f} minutes")
+        else:
+            print(f"Conversion took {conversion_time_seconds / 60 / 60:.2f} hours")
+
+    # Calculate thresholding events
+    if add_thresholding_events:
+        if verbose:
+            start_time = time.time()
+            print("Calculating and storing thresholding events with parameters: ")
+            print(thresholindg_pipeline_kwargs)
+
+        f_notch = thresholindg_pipeline_kwargs.get("f_notch", None)
+        bandwidth = thresholindg_pipeline_kwargs.get("bandwidth", None)
+        f_low = thresholindg_pipeline_kwargs.get("f_low", None)
+        f_high = thresholindg_pipeline_kwargs.get("f_high", None)
+        noise_threshold = thresholindg_pipeline_kwargs.get("noise_threshold", None)
+        job_kwargs = thresholindg_pipeline_kwargs.get("job_kwargs", None)
+
+        if add_raw_amplifier_data:
+            file_path = nwbfile_path
+        else:
+            file_path = intan_file_path
+
+        sorting = calculate_thresholding_events(
+            file_path=file_path,
+            f_notch=f_notch,
+            bandwidth=bandwidth,
+            f_low=f_low,
+            f_high=f_high,
+            noise_threshold=noise_threshold,
+            job_kwargs=job_kwargs,
+            stub_test=stub_test,
+            verbose=verbose,
+            probe_info_path=probe_info_path,
+        )
+
+        nwbfile_path = write_thresholding_events_to_nwb(
+            sorting=sorting,
+            nwbfile_path=nwbfile_path,
+            verbose=verbose,
+            thresholindg_pipeline_kwargs=thresholindg_pipeline_kwargs,
+        )
+
+        del sorting
+
+        if verbose:
+            stop_time = time.time()
+            thresholding_time = stop_time - start_time
+            if thresholding_time <= 60 * 3:
+                print(f"Thresholding events took {thresholding_time:.2f} seconds")
+            elif thresholding_time <= 60 * 60:
+                print(f"Thresholding events took {thresholding_time / 60:.2f} minutes")
+            else:
+                print(f"Thresholding events took {thresholding_time / 60 / 60:.2f} hours")
+
+    # Add PSTH
+    if add_thresholding_events and add_psth:
+        if verbose:
+            start_time = time.time()
+            print("Calculating and storing PSTH with parameters: ")
+            print(psth_kwargs)
+
+        number_of_bins = psth_kwargs.get("num_bins")
+        bins_span_milliseconds = psth_kwargs.get("bins_span_milliseconds")
+        bin_width_in_milliseconds = bins_span_milliseconds / number_of_bins
+        milliseconds_from_event_to_first_bin = psth_kwargs.get("milliseconds_from_event_to_first_bin", None)
+
+        write_binned_spikes_to_nwbfile(
+            nwbfile_path=nwbfile_path,
+            bin_width_in_milliseconds=bin_width_in_milliseconds,
+            number_of_bins=number_of_bins,
+            milliseconds_from_event_to_first_bin=milliseconds_from_event_to_first_bin,
+            verbose=verbose,
+        )
+
+        if add_psth_in_pipeline_format_to_nwb:
+            write_psth_pipeline_format_to_nwbfile(
+                nwbfile_path=nwbfile_path,
+                verbose=verbose,
+            )
+
+        if verbose:
+            stop_time = time.time()
+            psth_time = stop_time - start_time
+            if psth_time <= 60 * 3:
+                print(f"PSTH calculation took {psth_time:.2f} seconds")
+            elif psth_time <= 60 * 60:
+                print(f"PSTH calculation took {psth_time / 60:.2f} minutes")
+            else:
+                print(f"PSTH calculation took {psth_time / 60 / 60:.2f} hours")
+
+    if verbose:
+        total_stop = time.time()
+        total_scrip_time = total_stop - total_start
+        if total_scrip_time <= 60 * 3:
+            print(f"Total script took {total_scrip_time:.2f} seconds")
+        elif total_scrip_time <= 60 * 60:
+            print(f"Total script took {total_scrip_time / 60:.2f} minutes")
+        else:
+            print(f"Total script took {total_scrip_time / 60 / 60:.2f} hours")
+
+        print("\n \n")
+    
+    return nwbfile_path
